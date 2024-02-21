466e80eb4164edfd79e8b8a867ddc29c02ebcbf3
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-02-21 21:56:00 +0000

    show job runners
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- .github/actions/detect-runner-js/index.ts
+++ .github/actions/detect-runner-js/index.ts
@@ -1,7 +1,42 @@
-const world = 'world';
+import { Octokit } from "@octokit/action";
+import { RestEndpointMethodTypes } from "@octokit/action";
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+type RunnerForJobs = {
+    githubHosted: string[]
+    slefHosted: string[]
+};
+type ListJobsForWorkflowResponseData = RestEndpointMethodTypes["actions"]["listJobsForWorkflowRun"]["response"]["data"];
+
+const selfHostedLabel = "self-hosted";
+
+export async function getRunnerForJobs(): Promise<RunnerForJobs> {
+    const octokit = new Octokit();
+    const [owner, repo] = process.env.GITHUB_REPOSITORY!.split("/");
+
+    const res = await octokit.actions.listJobsForWorkflowRun({
+        owner: owner,
+        repo: repo,
+        run_id: Number(process.env.GITHUB_RUN_ID!),
+    });
+    const data: ListJobsForWorkflowResponseData = res.data;
+
+    const runnerForJobs: RunnerForJobs = {
+        githubHosted: [],
+        slefHosted: [],
+    }
+    data.jobs.forEach((job) => {
+        const targetList = job.labels.includes(selfHostedLabel) ? runnerForJobs.slefHosted : runnerForJobs.githubHosted;
+        targetList.push(job.name);
+    })
+
+    return runnerForJobs;
 }
 
+console.log(getRunnerForJobs());
+
+
+const world = 'world';
+export function hello(who: string = world): string {
+    return `Hello ${who}! `;
+};
 console.log(hello());
