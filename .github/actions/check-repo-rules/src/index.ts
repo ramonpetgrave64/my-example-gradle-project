b0ecea3b79890cd5c54b76e154dc1b47ab7c7820
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-03-14 20:31:00 +0000

    attempt at listing rules
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

---
+++ .github/actions/check-repo-rules/src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * The entrypoint for the action.
+ */
+import { run } from './main'
+import { Octokit } from '@octokit/rest'
+
+export async function go(): Promise<void> {
+  const [owner, repo] = `${process.env.GITHUB_REPOSITORY}`.split('/')
+  const octokit = new Octokit()
+  const branch = (await octokit.repos.get({ owner, repo })).data.default_branch
+  const rules = octokit.paginate(octokit.repos.getBranchRules, {
+    owner,
+    repo,
+    branch
+  })
+  console.dir(rules, { depth: 10 })
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+run()
+go()
